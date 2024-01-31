80f6df9fd957d1facd45d6a64f9b23a631bf51c4
uehara <uehara@isi.imi.i.u-tokyo.ac.jp>
2024-01-31 14:19:03 +0900

    :+1: updated source

--- src/script/base/whisperTranscript.py
+++ src/script/base/whisperTranscript.py
@@ -4,71 +4,22 @@
 import os
 import sys
 import json
+import yaml
 import ffmpeg
 import whisper
 import argparse
-#from pyannote.audio import Audio
-#from pyannote.audio import Pipeline
 
+sys.path.append("./src/library")
+
+from core.whisperanscript import Whisperancript
 
 parser = argparse.ArgumentParser(add_help=True)
 
-parser.add_argument('-f', '--file-name', dest="file_name", type=str, default='')
-parser.add_argument('-m', '--model', default='base')
-parser.add_argument("-l", "--language", type=str, default='en')
+parser.add_argument('--file-name', type=str, required=True)
+parser.add_argument('--destination', type=str, required=True)
+parser.add_argument('--model', default='base')
+parser.add_argument("--language", type=str, default='en')
 args = parser.parse_args()
-
-class Whisperancript:
-	def __init__(self, file_name):
-		self.filename = file_name
-		self.basename = os.path.splitext(os.path.basename(self.filename))[0] # USAGE: /sample/test.mov --> test
-		os.makedirs(f"../result/{self.basename}", exist_ok=True)
-
-		self.wav_filename = f"../result/{self.basename}/{self.basename}.wav"
-		self.json_filename = f"../result/{self.basename}/{self.basename}.json"
-		self.txt_filename = f"../result/{self.basename}/{self.basename}.txt"
-
-	def any2wav(self):
-		stream = ffmpeg.input(self.filename)
-		stream = ffmpeg.output(stream, self.wav_filename)
-		ffmpeg.run(stream)
-		return self.wav_filename
-
-
-
-	def load_transcript(self, lang = 'en'):
-		raw_file_name = self.filename
-		json_file_name = self.json_filename
-
-		if not os.path.isfile(json_file_name):
-			print('Now transcribing...')
-			model = whisper.load_model(args.model)
-			result = model.transcribe(self.wav_filename, verbose=False, language=lang)
-
-			with open(json_file_name, 'w') as f:
-				json.dump(result, f, indent=4)
-			print('Finish transcribing...')
-		else:
-			print(json_file_name + " already exsits.")
-			with open(json_file_name, 'r') as f:
-				result = json.load(f)
-			
-		#print(result['text'])
-		return result, raw_file_name
-
-	def print_lines(self, json_dictionary):
-		txt_file_name = self.txt_filename
-		
-		print('Now printing...')
-		f = open(txt_file_name, 'w')
-		for i in range(len(json_dictionary['segments'])):
-			item = json_dictionary['segments'][i]['text']
-			print(item)
-			f.writelines(item + '\n\n')
-		f.close()
-		print('Finish printing...')
-	
-		
 
 if __name__ == '__main__':
 	model = Whisperancript(args.file_name)
